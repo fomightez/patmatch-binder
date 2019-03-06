be1b66c7a3677d77592722d108e961080f482d37
fomightez <wayne4.antispam@server.fake>
2019-03-06 12:18:38 -0500

    add several minor improvements mostly for consistency with binder version

--- vice/entry.sh
+++ vice/entry.sh
@@ -2,4 +2,5 @@
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 cd $HOME
+cd patmatch-binder
 exec jupyter lab --no-browser
