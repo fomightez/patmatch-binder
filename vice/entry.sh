63e9c0da8d134c9a863dc6131460e7f8ad01ef62
fomightez <wayne4.antispam@server.fake>
2019-05-02 14:32:33 -0400

    add getting the companion content to the VICE entry.sh

--- vice/entry.sh
+++ vice/entry.sh
@@ -3,4 +3,11 @@
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 cd $HOME
 cd patmatch-binder
+# retrieve notebooks and place correctly in session
+rm index.ipynb
+git clone https://github.com/fomightez/ptmbr-accompmatz.git
+cp ptmbr-accompmatz/index.ipynb .
+mv ptmbr-accompmatz/notebooks .
+rm -rf ptmbr-accompmatz
+
 exec jupyter lab --no-browser
